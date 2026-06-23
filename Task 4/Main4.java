ac0d2b1b6c01b38c8111679bc6765947e224fa0b
Md. Masudul Hasan Akib <136964348+ak1bhasan@users.noreply.github.com>
2026-06-23 11:27:57 +0600

    Implement MessageSender interface with Email and SMS

--- Task 4/Main4.java
+++ Task 4/Main4.java
@@ -1,1 +1,56 @@
+interface MessageSender {
+    
+    void sendMessage(String message);
+}
 
+
+class EmailSender implements MessageSender {
+
+    public void sendMessage(String message) {
+        
+        System.out.println("Sending email: " + message);
+    }
+}
+
+
+class SMSSender implements MessageSender {
+
+    public void sendMessage(String message) {
+        
+        System.out.println("Sending SMS: " + message);
+    }
+}
+
+
+class NotificationService {
+
+    private MessageSender sender;
+
+    public NotificationService(MessageSender sender) {
+        this.sender = sender;
+    }
+
+    public void alertUser(String msg) {
+        sender.sendMessage(msg);
+    }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MessageSender email = new EmailSender();
+        
+        NotificationService emailNotification = new NotificationService(email);
+
+        emailNotification.alertUser("Your order has been shipped.");
+
+
+        MessageSender sms = new SMSSender();
+        
+        NotificationService smsNotification = new NotificationService(sms);
+
+        smsNotification.alertUser("Your OTP is 1234.");
+    }
+}
