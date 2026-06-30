834eecea0188dcc82522f8e994c6075cf9cf10fd
Md. Masudul Hasan Akib <136964348+ak1bhasan@users.noreply.github.com>
2026-06-30 11:37:50 +0600

    Implement disaster alert system with Bluetooth support

--- Task 5/Main7.java
+++ Task 5/Main7.java
@@ -1,1 +1,31 @@
+interface INetworkProtocol {
+    void broadcastMessage(String msg);
+}
 
+class BluetoothMeshNetwork implements INetworkProtocol{
+    public void broadcastMessage(String msg) {
+        System.out.println("Broadcasting via Bluetooth Mesh : " + msg);
+    }
+}
+
+class DisasterAlertSystem {
+    private INetworkProtocol network;
+
+    public DisasterAlertSystem(INetworkProtocol network) {
+        this.network = network;
+    }
+
+    public void triggerAlert(String alertData) {
+        network.broadcastMessage(alertData);
+    }
+}
+
+public class Main7 {
+    public static void main(String[] args) {
+        INetworkProtocol bluetoothNetwork = new BluetoothMeshNetwork();
+        DisasterAlertSystem alertSystem = new DisasterAlertSystem(bluetoothNetwork);
+
+        System.out.println("Disaster Alert System");
+        alertSystem.triggerAlert("SEVERE WEATHER WARNING: Evacuate immediately!");
+    }
+}
