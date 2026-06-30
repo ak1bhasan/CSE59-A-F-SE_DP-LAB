717ffe6e756a4fdff8ac81dbec3cdf5448a08130
Md. Masudul Hasan Akib <136964348+ak1bhasan@users.noreply.github.com>
2026-06-30 11:37:16 +0600

    Add university member classes and research interface

--- Task 5/Main5.java
+++ Task 5/Main5.java
@@ -1,1 +1,33 @@
+class UniversityMember {
+    public void attendLecture() {
+        System.out.println("Attending a lecture.");
+    }
+}
 
+interface Researcher {
+    void conductLabResearch();
+}
+
+class UndergraduateStudent extends UniversityMember {
+
+}
+
+class GraduateResearchAssistant extends UniversityMember implements Researcher {
+    public void conductLabResearch() {
+        System.out.println("Publishing papers and running experiments.");
+    }
+}
+
+public class Main5 {
+    public static void main(String[] args) {
+        UniversityMember undergrad = new UndergraduateStudent();
+        GraduateResearchAssistant grad = new GraduateResearchAssistant();
+
+        System.out.println("Undergraduate");
+        undergrad.attendLecture();
+
+        System.out.println("Graduate Research Assistant");
+        grad.attendLecture();
+        grad.conductLabResearch();
+    }
+}
