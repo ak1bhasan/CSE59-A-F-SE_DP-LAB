76f4aace1176d8bc4d2c15e82f205f7cbb6922f0
Md. Masudul Hasan Akib <136964348+ak1bhasan@users.noreply.github.com>
2026-06-30 11:37:38 +0600

    Add interfaces and WebController implementation

--- Task 5/Main6.java
+++ Task 5/Main6.java
@@ -1,1 +1,26 @@
+interface HttpService {
+    void handleHttpRequest();
+}
 
+interface DatabaseManager {
+    void executeSqlStatement();
+}
+
+interface ContainerOps {
+    void restartDockerContainer();
+}
+
+class WebController implements HttpService {
+    public void handleHttpRequest() {
+        System.out.println("Routing traffic to endpoint.");
+    }
+}
+
+public class Main6 {
+    public static void main(String[] args) {
+        WebController controller = new WebController();
+        
+        System.out.println("Web Controller");
+        controller.handleHttpRequest();
+    }
+}
